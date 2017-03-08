1387d0f79d59c59ecece0a47241594aea2ae0982
Marc Kun <thelegendarymarc@gmail.com>
2017-03-08 18:54:07 +0100

    Recreate Project in VS17

--- NitradoMAI/main.Designer.cs
+++ NitradoMAI/main.Designer.cs
@@ -35,10 +35,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(749, 516);
+            this.ClientSize = new System.Drawing.Size(825, 572);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "main";
-            this.Text = "NitradoMAI";
+            this.Text = "Nitrado MAI";
             this.ResumeLayout(false);
 
         }
